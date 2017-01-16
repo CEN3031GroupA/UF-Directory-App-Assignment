18fe6a0d3f123c3de2f5fedb49db40af1628a314
Clinton Andrews <candrews800@gmail.com>
2017-01-15 23:38:29 -0500

    complete task items

--- listingController.js
+++ listingController.js
@@ -3,12 +3,18 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function(item) {
+      $scope.listings.push(item);
+    };
+    $scope.deleteListing = function(item) {
+      for (var i = 0; i < $scope.listings.length; i++) {
+        if ($scope.listings[i].code === item.code) {
+          $scope.listings.splice(i, 1);
+        }
+      }
+    };
+    $scope.showDetails = function(item) {
+      $scope.detailedInfo = item;
+    };
   }
 ]);
